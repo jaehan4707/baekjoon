91e35f2eabd899d49360b0a23e8f9cabb338aae2
Leejaehan <jahan4707@naver.com>
2022-03-07 00:18:25 +0900

    nine commit

--- 1012(220210)/1012(220210)/배추.c
+++ 1012(220210)/1012(220210)/배추.c
@@ -7,26 +7,27 @@ int dfs(int x, int y);
 int ary[50][50];
 int main()
 {
+	int i, j, k, h;
 	int t=0,Obj=0,Row=0,Col=0,count=0;
 	scanf("%d",&t);
-	for (int i = 0; i < t; i++)
+	for (i = 0; i < t; i++)
 	{
 		scanf("%d %d %d",&Maxrow,&Maxcol,&Obj);
 		if (Maxrow > 50 || Maxrow <1 || Maxcol<1 || Obj<0 ||Maxcol > 50 || Obj > 2500)
 			exit(1);
 		memset(ary, 0, sizeof(ary));// 최대 가로길이,세로길이 배추개수 
-		for (int j = 0; j < Obj; j++) 
+		for (j = 0; j < Obj; j++) 
 		{
 			scanf("%d %d",&Row,&Col);
 			ary[Row][Col] = 1; // 배추 잇는 곳은 1로 초기화
 		}
-		for (int x = 0; x< Maxrow; x++)
+		for (k = 0; k< Maxrow; k++)
 		{
-			for (int y = 0; y < Maxcol; y++)
+			for (h = 0; h < Maxcol; h++)
 			{
-				if (ary[x][y] == 1)
+				if (ary[k][h] == 1)
 				{
-					dfs(x, y);
+					dfs(k, h);
 					count++;
 				}
 			}
@@ -36,37 +37,44 @@ int main()
 	}
 	return 0;
 }
-/*
-int dfs(int row, int col)
-{
-	ary[row][col] = 0;
-	if (row-1>=0&& ary[row - 1][col] == 1) //up 
-		dfs(row-1, col);
-	if (row+1<Maxrow && ary[row + 1][col] == 1) //down
-		dfs(row+1, col);
-	if (Maxcol-1>=0&& ary[row][col - 1]==1) //left
-		dfs(row, col-1);
-	if (col+1<Maxcol&& ary[row][col + 1] == 1) //right
-		dfs(row, col+1); 
-	return 0;
-}
-*/
-int dfs(int x, int y) {
 
-    ary[x][y] = 0;
+
+int dfs(int x, int y){
+
+	ary[x][y] = 0;
 	if (x + 1 < Maxrow && ary[x + 1][y] == 1)
-	{
+	{ //down
 		dfs(x + 1, y);
 	}
 	if (x - 1 >= 0 && ary[x - 1][y] == 1)
+	{ //up 
+		dfs(x - 1, y);
+	}
+	if (y + 1 < Maxcol && ary[x][y + 1] == 1) { //right
+		dfs(x, y + 1);
+	}
+	if (y - 1 >= 0 && ary[x][y - 1] == 1) { //left
+		dfs(x, y - 1);
+	}
+	return 0;
+}
+
+/*int dfs(int x, int y) {
+
+    ary[x][y] = 0;
+	if (x + 1 < Maxrow && ary[x + 1][y] == 1) //Down
+	{
+		dfs(x + 1, y);
+	}
+	if (x - 1 >= 0 && ary[x - 1][y] == 1) // Up
 	{
 		dfs(x - 1, y);
 	}
-	if (y + 1 < Maxcol && ary[x][y + 1] == 1) {
+	if (y + 1 < Maxcol && ary[x][y + 1] == 1) { //Right
 		dfs(x, y + 1);
 	}
-	if (y - 1 >= 0 && ary[x][y - 1] == 1) {
+	if (y - 1 >= 0 && ary[x][y - 1] == 1) { //Left
 		dfs(x, y - 1);
 	}
     return 0;
-}
+}*/
