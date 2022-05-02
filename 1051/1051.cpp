c4120f4348eef7fd026c568763dfc86c84e8c885
Leejaehan <jahan4707@naver.com>
2022-05-03 01:18:55 +0900

    1051 +

---
+++ 1051/1051.cpp
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int>r;
+vector<int>c;
+vector<int>a, b;
+int search(vector<vector<int>>& a, int row, int col);
+int main()
+{
+	cin >> n >> m;
+	vector<vector<int>>ary(n, vector<int>(m, 0));
+	vector<int>Max;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			scanf("%1d", &ary[i][j]);
+		}
+	}
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 0; j < m-1; j++)
+		{
+			int temp = search(ary, i, j);
+			if (temp == 0)
+				continue;
+			else
+				Max.push_back(temp);
+		}
+	}
+	if (Max.size()==0)
+	{
+		cout << 1;
+	}
+	else
+	{
+		sort(Max.begin(), Max.end());
+		cout << Max[Max.size() - 1];
+	}
+}
+int search(vector<vector<int>>& a,int row, int col) //한축 잡고 꼭짓점 잡아줌.
+{
+	int edge = a[row][col];
+	int size = 0, len=0, idx = 0, ans = 0;
+	for (int j = col + 1; j < m; j++)
+	{
+		if (edge == a[row][j])
+			c.push_back(j);
+	}
+	size = c.size();
+
+	while (size--)
+	{
+		len = c[idx] - col;
+		idx++;
+		if (row + len >= n || col + len >= m)
+			continue;
+		if (edge == a[row + len][col] && edge == a[row + len][col + len])
+			if (ans < len + 1)
+				ans = len + 1;
+	}
+	c.clear();
+	return ans * ans;
+	
+}
--- 2667/2667.cpp
+++ 2667/2667.cpp
@@ -1,41 +1,86 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <vector>
-
+#include <stack>
+#include <string>
+#include <algorithm>
 using namespace std;
-vector<int> F;
-vector<pair<int, int>> p;
-int n, c = 0, C = 0;
-void fibo(int n, int& a, int& b);
+stack<int> s;
+int n, cnt = 0, c = 0;
+void move(int row, int col, vector<vector<int>>& graph, vector<vector<int>>& visit);
 int main()
 {
-    F.push_back(0);
-    F.push_back(1);
-    p.push_back(make_pair(1, 0));
-    p.push_back(make_pair(0, 1));
     cin >> n;
-    int t, a = 0, b = 0;
+    vector<vector<int>> graph(n, vector<int>(n, 0));
+    vector<vector<int>> visit(n, vector<int>(n, 0));
+    vector<int> house;
     for (int i = 0; i < n; i++)
     {
-        cin >> t;
-        fibo(t, a, b);
-        cout << a << " " << b << endl;
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%1d", &graph[i][j]); //숫자 하나씩 넣기
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (graph[i][j] == 1 && visit[i][j] == 0) //집이 있고,방문하지 않은곳
+            {
+                cnt++;     //단지 수를 나타냄.
+                s.push(0); //단순 스택 채우기용 push, 안에 숫자는 별 내용 없음
+                move(i, j, graph, visit);
+                while (s.empty() == 0) // stack size가 단지 내 집의 수
+                {
+                    s.pop();
+                    c++;
+                }
+                house.push_back(c); // pop한 횟수가 집의 수 집의 수를 배열에 저장.
+                c = 0;
+            }
+        }
+    }
+    cout << cnt << endl;              // 단지 수 출력
+    sort(house.begin(), house.end()); // house배열을 오름차순으로 정렬
+    for (int i = 0; i < house.size(); i++)
+    {
+        cout << house[i] << endl; // house배열을 출력
     }
 }
-void fibo(int n, int& a, int& b)
+void move(int row, int col, vector<vector<int>>& graph, vector<vector<int>>& visit)
 {
-    if (n < 2)
+    visit[row][col] = 1; //현재 좌표를 방문했다고 표시
+    //각 좌표마다 갈 수 있는지 검사후 움직여줌.
+    if (row + 1 < n)
     {
-        a = p[n].first;
-        b = p[n].second;
-    }
-    else
-    {
-        for (int i = 2; i < n; i++)
+        if (graph[row + 1][col] == 1 && visit[row + 1][col] == 0)
         {
-            F.push_back(F[i - 2] + F[i - 1]);
-            p.push_back(make_pair(p[i - 1].first + p[i - 2].second, p[i - 1].second + p[i - 2].second));
+            s.push(0);
+            move(row + 1, col, graph, visit);
         }
-        a = p[n].first;
-        b = p[n].second;
+    }
+    if (row - 1 >= 0)
+    {
+        if (graph[row - 1][col] == 1 && visit[row - 1][col] == 0)
+        {
+            s.push(0);
+            move(row - 1, col, graph, visit);
+        }
+    }
+    if (col + 1 < n)
+    {
+        if (graph[row][col + 1] == 1 && visit[row][col + 1] == 0)
+        {
+            s.push(0);
+            move(row, col + 1, graph, visit);
+        }
+    }
+    if (col - 1 >= 0)
+    {
+        if (graph[row][col - 1] == 1 && visit[row][col - 1] == 0)
+        {
+            s.push(0);
+            move(row, col - 1, graph, visit);
+        }
     }
 }
