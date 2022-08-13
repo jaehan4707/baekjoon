9b3b96a1dab186670e405be00f08fa2a31647189
Leejaehan <jahan4707@naver.com>
2022-08-13 21:25:55 +0900

    solve 1916 최소비용구하기

---
+++ Dijkstra/17396.cpp
@@ -0,0 +1,9 @@
+/*
+백준 골드5 백도어 
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main(){
+}
---
+++ Dijkstra/1753.cpp
@@ -0,0 +1,12 @@
+/*
+백준 실버1 지름길 구하기
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+
+}
