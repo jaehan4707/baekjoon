3eda94307a8f1c25e436b15b4a5f2591ea8f47f4
Leejaehan <jahan4707@naver.com>
2022-08-16 17:37:42 +0900

    solve 1552 빠른 A+B

---
+++ 15552.cpp
@@ -0,0 +1,17 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+     cin.tie(nullptr);
+    cout.tie(nullptr);
+    int n,a,b;
+    cin>>n;
+    for(int i=0; i<n;i++){
+        cin>>a>>b;
+        cout<<a+b<<'\n';
+    }
+}
