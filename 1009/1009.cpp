a25f67accd23dacb4ed05d21dad506ee248a35ec
Leejaehan <jahan4707@naver.com>
2022-09-02 20:43:42 +0900

    modify 9657 돌게임3

---
+++ 1009/1009.cpp
@@ -0,0 +1,18 @@
+/*
+백준 브론즈 2 1009 분산처리
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+int n, a, b;
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b;
+    }
+}
