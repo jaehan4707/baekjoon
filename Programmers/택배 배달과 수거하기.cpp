8102b20fcd5c4624476c3b9260f95f96b188558e
jaehan4707 <jahan4707@naver.com>
2023-03-13 19:01:06 +0900

    modify 택배 배달과 수거하기

--- Programmers/택배 배달과 수거하기.cpp
+++ Programmers/택배 배달과 수거하기.cpp
@@ -45,35 +45,25 @@ void funct(vector<int> &deliveries, vector<int> &pickups)
             deliveries.pop_back();
             continue;
         }
-        if (tempD == lim)
-        { // 꽉꽉 채웠슴.
+        if (tempD == lim) //꽉 채운 경우
+        {
             break;
         }
         if (deliveries[i] != 0)
         { // 배달할 상자가 있다면.
             check = true;
-            // cout<<i+1<<" 배달할 상자가 있습니다"<<endl;
             if (lim < tempD + deliveries[i])
             { // 현재 트럭에 있는 상자수보다 더 필요하다면.
-                // cout<<"현재 트럭에 있는 상자수 보다 배달할 상자가 더 많습니다 "<<endl;
-                // cout<<"배달전 : "<<deliveries[i]<<" ";
-                // total_d -= lim-tempD; //담은만큼.
                 deliveries[i] -= lim - tempD;
                 tempD += lim - tempD; //
-                                      // cout<<"배달 후 "<<deliveries[i]<<endl;
             }
             else // 현재 트럭에 있는 상자수로 해결된다면
             {
-                // cout<<"현재 트럭에 있는 상자수로 배달이 됩니다."<<endl;
-                // cout<<"배달 전 : "<<tempD<<" ";
                 tempD += deliveries[i];
-                //  total_d-=deliveries[i];
-                // cout<<"배달 후 : "<<tempD<<endl;
                 deliveries[i] = 0;
             }
             if (deliveries[i] == 0)
-            {
-                // cout<<i+1<<"단계는 완료했습니다"<<endl;
+            {        
                 deliveries.pop_back();
                 check = false;
             }
@@ -94,25 +84,15 @@ void funct(vector<int> &deliveries, vector<int> &pickups)
         if (pickups[i] != 0)
         { // 픽업할 상자가 있다면.
             check = true;
-            // cout<<i+1<<" 픽업할 상자가 있습니다"<<endl;
             if (tempP + pickups[i] > lim)
             {
-                // 현재 트럭에 있는 상자수 + 수거한 상자수가 크다면. 일단 할 수 있는 만큼 담는다
-                // cout<<"현재 트럭에는 상자가 많습니다 "<<tempP<<endl;
-                // cout<<"수거 전 : "<<pickups[i];
-                // total_p -= (lim- tempP);
-                // total_p -= lim-tempP;
+                // 현재 트럭에 있는 상자수 + 수거한 상자수가 크다면. 일단 할 수 있는 만큼 담는다.
                 pickups[i] -= (lim - tempP); // 담은 만큼 빼준다.
-                // cout<<"수거 후 : "<<pickups[i]<<endl;
                 tempP = lim; // 최대로 찻음.
             }
             else
             { // 방문한곳에서 전부다 수거 할 수 있으면.
-                //  cout<<"현재 트럭에는 여유공간이 많습니다"<<endl;
-                // cout<<"수거 전 : "<<tempP;
                 tempP += pickups[i];
-                // cout<<"수거 후 : "<<tempP<<endl;
-                // total_p -= pickups[i];
                 pickups[i] = 0;
             }
             if (pickups[i] == 0)
